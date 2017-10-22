dbdbfc9a170cdca0f6db8dc0d8376cafab0f19b7
Michael <info1117@gmail.com>
2017-10-22 07:52:13 -0700

    Improves structure

--- backend_node_firebase/notification.js
+++ backend_node_firebase/notification.js
@@ -10,10 +10,4 @@ admin.initializeApp({
 });
 
 const pango = new Pango(admin);
-pango.deleteRecords();
-//pango.addRecords(defaultData.records);
-pango.addRecords(defaultData.transactions, "transactions");
-pango.addRecord(defaultData.profile, "profile");
 pango.addReward(defaultData.newRecord);
-console.log(pango.indexRewards(defaultData.records).map(a => a.type));
-pango.addRecords(pango.indexRewards(defaultData.records));
